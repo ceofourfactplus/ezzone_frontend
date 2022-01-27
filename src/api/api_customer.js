1ddf619c9f1100d59a3d4f94728d1b92e6875850
ceo <ceofourfactplus@gmail.com>
2022-01-28 00:51:37 +0700

    change api

--- src/api/api_customer.js
+++ src/api/api_customer.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api_customer = axios.create({
-  baseURL: 'http://192.168.1.118:8000/customer',
+  baseURL: 'http://192.46.227.22:8000/customer',
   timeout: 1000,
 });
 
