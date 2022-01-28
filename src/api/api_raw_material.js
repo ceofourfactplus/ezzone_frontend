252202dc41aacf8eef5504ac7016451fa1aac28c
ceo <ceofourfactplus@gmail.com>
2022-01-28 12:25:05 +0700

    update path

--- src/api/api_raw_material.js
+++ src/api/api_raw_material.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api_raw_material = axios.create({
-    baseURL: 'http://http://192.46.227.22:8000/raw_material',
+    baseURL: 'http://192.46.227.22:8000/raw_material',
 })
 
 export{ api_raw_material }
