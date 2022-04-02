1731e64f5fd4b53ef40d6963a2edec3d19ad3cfb
siri <sirifourfactplus@gmail.com>
2022-04-02 18:35:30 +0700

    Update Report Daily (last day on month)

--- src/views/Report/MainReport.vue
+++ src/views/Report/MainReport.vue
@@ -204,6 +204,7 @@
 import moment from "moment";
 import { api_pos } from "../../api/api_pos";
 import NavApp from "../../components/main_component/NavApp.vue";
+import axios from 'axios';
 export default {
   components: { NavApp },
   mounted() {
@@ -236,14 +237,9 @@ export default {
       var data = new FormData();
       this.day = dateObj.getDay();
       if (this.$route.params.type == 'daily') {
-        data.append("year_to", year)
-        data.append("month_to", month)
-        data.append("day_to", day + 1)
-        data.append("year_from", year)
-        data.append("month_from", month)
-        data.append("day_from", day)
-        this.from_date = `${year}-${month}-${day}`
-        this.to_date = `${year}-${month}-${day + 1}`
+        api_pos.get('report/daily').then(response => {
+          this.report = response.data
+        })
       } else if (this.$route.params.type == 'weekly') {
         this.calc_week()
         day -= this.day
@@ -253,6 +249,11 @@ export default {
         data.append("year_from", year)
         data.append("month_from", month)
         data.append("day_from", day)
+        api_pos
+          .post("report/by_date", data)
+          .then((response) => {
+            this.report = response.data;
+        });
       } else {
         data.append("year_to", year)
         data.append("month_to", month + 1)
@@ -260,12 +261,13 @@ export default {
         data.append("year_from", year)
         data.append("month_from", month)
         data.append("day_from", day)
-      }
-      api_pos
+        api_pos
           .post("report/by_date", data)
           .then((response) => {
             this.report = response.data;
         });
+      }
+      
     },
     find() {
       if(this.from_date != '' && this.to_date != '') {
